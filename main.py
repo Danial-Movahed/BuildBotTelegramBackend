e9e6b909770898c43a97159a3191979cf25c3f1e
Seyed Danial Movahed <movaheddanial@gmail.com>
2024-01-12 09:33:36 +0330

    Added loading project

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, Me
 from credentials import BOT_TOKEN, BOT_USERNAME
 import json
 
+LoadedProjects = {}
 
 async def SetupAll(update: Update, callback: CallbackContext):
     await update.get_bot().set_chat_menu_button(
@@ -13,29 +14,33 @@ async def SetupAll(update: Update, callback: CallbackContext):
             text="Open menu")
     )
 
-    inl = [
-        [
-            InlineKeyboardButton(
-                "Open Menu",
-                web_app=WebAppInfo("https://danial-movahed.github.io/")
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                "Help commands",
-                web_app=WebAppInfo("https://danial-movahed.github.io/help/commands")
-            )
-        ]
+    kb = [
+        [KeyboardButton(
+            "Open Menu",
+            web_app=WebAppInfo("https://danial-movahed.github.io/")
+        )],
+        [KeyboardButton(
+            "Help commands",
+            web_app=WebAppInfo(
+                "https://danial-movahed.github.io/help/commands")
+        )]
     ]
 
     await update.message.reply_text(
         "Hello. Welcome to Danial's build bot. Check out menu for a gui or help for list of commands.",
-        reply_markup=InlineKeyboardMarkup(inl)
+        reply_markup=ReplyKeyboardMarkup(kb)
     )
+
+async def web_app_data(update: Update, context: CallbackContext):
+    data = json.loads(update.message.web_app_data.data)
+    if data["type"] == "Load":
+        LoadedProjects[update.effective_user.id] = data["Project"]
+        await update.message.reply_text(f"Loaded project {data['Project']}")
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler('start', SetupAll))
+    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
     print(
         f"Your bot is listening! Navigate to https://t.me/{BOT_USERNAME} to interact with it!")
     application.run_polling()
