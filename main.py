cc518213ef3245f6bfe411e7596b704fd36b5f68
Seyed Danial Movahed <movaheddanial@gmail.com>
2024-01-12 11:09:13 +0330

    Added project loaded menu

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 
 LoadedProjects = {}
 
+
 async def SetupAll(update: Update, callback: CallbackContext):
     await update.get_bot().set_chat_menu_button(
         chat_id=update.effective_chat.id,
@@ -31,16 +32,33 @@ async def SetupAll(update: Update, callback: CallbackContext):
         reply_markup=ReplyKeyboardMarkup(kb)
     )
 
+
 async def web_app_data(update: Update, context: CallbackContext):
     data = json.loads(update.message.web_app_data.data)
     if data["type"] == "Load":
         LoadedProjects[update.effective_user.id] = data["Project"]
-        await update.message.reply_text(f"Loaded project {data['Project']}")
+        kb = [
+            [KeyboardButton(
+                "Open Menu",
+                web_app=WebAppInfo("https://danial-movahed.github.io/menus/projectloaded.html")
+            )],
+            [KeyboardButton(
+                "Help commands",
+                web_app=WebAppInfo(
+                    "https://danial-movahed.github.io/help/commands")
+            )]
+        ]
+        await update.message.reply_text(
+            f"Loaded project {data['Project']}",
+            reply_markup=ReplyKeyboardMarkup(kb)
+        )
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler('start', SetupAll))
-    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
+    application.add_handler(MessageHandler(
+        filters.StatusUpdate.WEB_APP_DATA, web_app_data))
     print(
         f"Your bot is listening! Navigate to https://t.me/{BOT_USERNAME} to interact with it!")
     application.run_polling()
